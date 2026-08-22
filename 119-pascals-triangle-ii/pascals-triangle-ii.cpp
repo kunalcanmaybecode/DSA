fd767ef42c321f846c5ca7ee0f479fdc2ed177f4
kunalcanmaybecode <kunal.bgsnps@gmail.com>
2026-08-22 19:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (5.76%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> dp(rowIndex+1, vector<int> (rowIndex+1,1));
+        for(int i = 1; i < rowIndex+1; i++){
+            for(int j = 1; j < i; j++){
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }
+        }
+
+        return dp[rowIndex];
+    }
+};
